b7c66b7833d735b4abbb3b23cd90e29ba1c8d6d8
Bryan <bryan@Bryans-MacBook-Pro.local>
2017-05-18 05:57:10 -0700

    commit with almost done

---
+++ Lesson 7 Video Game/AddCharacterViewController.swift
@@ -0,0 +1,129 @@
+//
+//  AddCharacterViewController.swift
+//  Lesson 7 Video Game
+//
+//  Created by Bryan on 5/16/17.
+//  Copyright © 2017 KO. All rights reserved.
+//
+
+import UIKit
+
+class AddCharacterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate { //13th and 14th
+    
+    
+    
+    @IBOutlet weak var characterImageView: UIImageView!    //10th
+    
+    
+    @IBOutlet weak var characterTextField: UITextField!     //10th
+    
+    
+    @IBOutlet weak var updateLabel: UIButton!   //47th  same thing as Add button
+    
+    
+    @IBOutlet weak var deleteLabel: UIButton!    //49th
+    
+    
+    
+    var imagePicker = UIImagePickerController()      //11th  uiimagepickercontroller lets you choose from collection or take a photo
+    
+    
+    var character : Character? = nil           //42nd
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self    //12th  where to send the image once sent. an object uses a delegate to pull some info that it needs
+        
+        if character != nil {  //46th
+            characterImageView.image = UIImage(data: character!.image! as Data)
+            characterTextField.text = character!.name
+            
+            updateLabel.setTitle("Update", for: .normal)   //48th  .setTitle for naming a button
+        } else {
+            deleteLabel.isHidden = true     //50th
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func photosTapped(_ sender: Any) {  //10th
+        
+        imagePicker.sourceType = .photoLibrary     //13th
+        
+        present(imagePicker, animated: false, completion: nil)    //14th this places another view controller on the screen for user to choose the pic
+        
+        
+        //15th click on info.plist. click information property list. plus button. Privacy Photo library for to gain user permission
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController,    didFinishPickingMediaWithInfo info: [String : Any]) {    //16th info is dictionary. function that gets called whenever user selects a photo. hold option on keyboard to look at documentation
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage   //17th UIImage is the data around an image file and we can use this to put it inside an Image View
+        
+        characterImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)    //18th shut down the image picker
+        
+    }
+    
+    
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {        //10th
+    }
+    
+    
+    @IBAction func addCharacter(_ sender: Any) {// 10th
+        
+        if character != nil {   //51st 
+            character!.name = characterTextField.text
+            character!.image = UIImagePNGRepresentation(characterImageView.image!)! as NSData
+            
+            
+            
+        } else {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext              //20th
+        
+        let character = Character(context: context)   //21st
+        character.name = characterTextField.text
+        character.image = UIImagePNGRepresentation(characterImageView.image!)! as NSData    //changing the nsdata to UIimage view
+            
+        }
+        
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()  //22nd
+        
+        navigationController!.popViewController(animated: true)
+        
+}
+    
+    
+
+        
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Lesson 7 Video Game/ViewController.swift
+++ Lesson 7 Video Game/ViewController.swift
@@ -8,18 +8,106 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource { //33rd
+    
+    
+    @IBOutlet weak var tableView: UITableView!     //25th
+    
+    var characters: [Character] = []    //26th
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tableView.dataSource = self      //31st
+        tableView.delegate = self     //32nd
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    override func viewWillAppear(_ animated: Bool) {   //27th
+        getCharacters()   //29th
+        tableView.reloadData()   //30th
+        
     }
-
-
+    
+    func getCharacters() {   //28th
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            characters = try context.fetch(Character.fetchRequest()) as! [Character]
+            print(characters)
+        } catch {
+            print("shit dont work")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {     // 34th
+        return characters.count    //35th
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {   //36th
+        let cell = UITableViewCell()
+        let character = characters[indexPath.row]     //37th
+        
+        cell.textLabel?.text = character.name! //38th
+        cell.imageView?.image = UIImage(data: character.image! as Data)   //40th  creates a UI image out of NS data. it wants just data and not ns data so also gotta put as Data
+        
+        return cell   //39th
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {        //41st
+        
+        let character = characters[indexPath.row]    //43rd
+        performSegue(withIdentifier: "characterSegue", sender: character)  //44th
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {  //45th
+        let nextVC = segue.destination as! AddCharacterViewController
+        nextVC.character = sender as? Character
+        
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+// embed in navigation controller 1st
+// add bar button item to top .  system item = add        2nd
+//control drag from plus button to other view controller eliminates need for perform segue 3rd
+// search and add navigation controller item to 2nd view controller and drag under vc scene under view 4th
+// add image view 5th
+// add text field 6th
+// add button 7th
+// click drag to files to add image to file. create groups.  click image view.  caarrot image. click name of file.  8th
+// create new cocoa touch view controller for code.  click yellow circle on storyboard and connect custom class to addcharactervc 9th
+
+
+
+
+//19th   add entity. for entity, click class codegen: class definition. add attributes name and image.  image type is binary data. click and then click on the shield and check allow external storage. uncheck optional for both
+
+
+//23rd add tableview to view controller storyboard. 0's for all, uncheck constrain margins box
+
+//24th get rid of adjust scroll inserts
+
+
+
 
